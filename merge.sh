400e93708595aa9d52289523420fc85b2bd7cae8
rmagur1203 <rmagur1203@naver.com>
2021-08-22 01:40:00 +0000

    move split files

--- merge.sh
+++ merge.sh
@@ -1,4 +1,4 @@
-cat files.db.seg_* > files.db
-rm files.db.seg_*
-cat data.db.seg_* > data.db
-rm data.db.seg_*
+cat splits/files.db.seg_* > files.db
+rm splits/files.db.seg_*
+cat splits/data.db.seg_* > data.db
+rm splits/data.db.seg_*
--- split.sh
+++ split.sh
@@ -1,3 +1,3 @@
-rm files.db.seg_* data.db.seg_*
-split -b 50m files.db files.db.seg_
-split -b 50m data.db data.db.seg_
+rm splits/files.db.seg_* splits/data.db.seg_*
+split -b 50m files.db splits/files.db.seg_
+split -b 50m data.db splits/data.db.seg_
