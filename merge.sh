a4b4aed306a2145e057e7724300a2d14592b0e2c
rmagur1203 <rmagur1203@naver.com>
2021-08-22 01:15:19 +0000

    update shell script

--- merge.sh
+++ merge.sh
@@ -1,2 +1,4 @@
 cat files.db.seg_* > files.db
 rm files.db.seg_*
+cat data.db.seg_* > data.db
+rm data.db.seg_*
--- split.sh
+++ split.sh
@@ -1,1 +1,3 @@
+rm files.db.seg_* data.db.seg_*
 split -b 50m files.db files.db.seg_
+split -b 50m data.db data.db.seg_
