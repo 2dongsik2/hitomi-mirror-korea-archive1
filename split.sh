d09a7e0fa700cefdbceeea86ad3714045e1a7575
rmagur1203 <rmagur1203@naver.com>
2021-08-22 05:46:04 +0000

    create checksum

--- split.sh
+++ split.sh
@@ -3,3 +3,5 @@ split -b 50m files.db splits/files.db.seg_
 split -b 50m data.db splits/data.db.seg_
 ls splits | grep -P "files.db.seg_.*" > splits/files.list
 ls splits | grep -P "data.db.seg_.*" > splits/data.list
+md5sum splits/*seg_* > splits/md5sum
+sha1sum splits/*seg_* > splits/sha1sum
