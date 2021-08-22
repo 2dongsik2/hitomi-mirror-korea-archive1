1fc40225c2cc24054b634e14f6c94c45930e8a3d
rmagur1203 <rmagur1203@naver.com>
2021-08-22 02:46:44 +0000

    create list file

--- split.sh
+++ split.sh
@@ -1,3 +1,5 @@
 rm splits/files.db.seg_* splits/data.db.seg_*
 split -b 50m files.db splits/files.db.seg_
 split -b 50m data.db splits/data.db.seg_
+ls splits | grep -P "files.db.seg_.*" > splits/files.list
+ls splits | grep -P "data.db.seg_.*" > splits/data.list
